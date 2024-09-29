ef963ca7117801ed40f8d7b0325d355322b7a378
Rachaud Keyes <rachaudkeyes@gmail.com>
2024-09-29 11:50:00 -0500

    Part 2 submission

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 DEFAULT_IMAGE_URL = "https://cdn.vectorstock.com/i/500p/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg"
@@ -29,8 +30,45 @@ class User(db.Model):
                           nullable=False,
                           default=DEFAULT_IMAGE_URL)
     
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+    
+    def __repr__(self):
+        return f"{self.id} {self.first_name} {self.last_name} {self.image_url}"
+    
     @property
     def full_name(self):
         """Return full name of user"""
 
         return f"{self.first_name} {self.last_name}"
+    
+    
+    
+
+class Post(db.Model):
+    """Blog post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    title = db.Column(db.Text,
+                      nullable=False)
+    
+    content = db.Column(db.Text,
+                        nullable=False)
+    
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+    
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
+    
+    @property
+    def friendly_date(self):
+        """Return nicely formatted date."""
+
+        return self.created_at.strftime("%a %b %-d %Y, %-I:%M %p")
